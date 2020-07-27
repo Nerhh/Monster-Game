73eb8417f33910b224f25054852b081fd9c95d87
Marcus Taylor-Deacon <marcus@deacondevs.com>
2020-07-27 21:55:44 +0100

    Heal + strong-attack implementation and refactored code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
-const attackValPlayer = 9; 
+const maxHeal = 6;
+const attackValPlayer = 9;
 const attackValMonster = 10;
+const strongAttackValPlayer = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,10 +9,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function onAttack() {
-    // Deal damage (random (val)) - UI updated 
-    const damageMonster = dealMonsterDamage(attackValPlayer);
-    currentMonsterHealth -= damageMonster;
+function deathCheck() {
     const damagePlayer = dealPlayerDamage(attackValMonster);
     currentPlayerHealth -= damagePlayer
 
@@ -23,4 +22,44 @@ function onAttack() {
     }
 }
 
+function attackMonster(attackMode) {
+    let maxDamage;
+
+    if (attackMode === 'attack') {
+        maxDamage = attackValPlayer;
+    } else if (attackMode === 'strong-attack') {
+        maxDamage = strongAttackValPlayer;
+    }
+
+    const damageMonster = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damageMonster;
+
+    deathCheck();
+}
+
+function onAttack() {
+    attackMonster('attack');
+}
+
+function onStrongAttack() {
+    attackMonster('strong-attack');
+}
+
+function onHeal() {
+    let healVal;
+
+    if (currentPlayerHealth >= chosenMaxLife - maxHeal) {
+        alert('You cannot heal above your max initial health');
+        healVal = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healVal = maxHeal;
+    }
+
+    increasePlayerHealth(maxHeal); 
+    currentPlayerHealth += maxHeal;
+    deathCheck();
+}
+
 attackBtn.addEventListener('click', onAttack);
+strongAttackBtn.addEventListener('click', onStrongAttack);
+healBtn.addEventListener('click', onHeal);
